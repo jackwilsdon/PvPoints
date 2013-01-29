5b5883f63bf455d1dab5957305c373939e3f3c99
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-01-29 21:42:53 +0000

    Removed unused includes

--- src/com/jackwilsdon/PvPoints/PvPointsPlayerManager.java
+++ src/com/jackwilsdon/PvPoints/PvPointsPlayerManager.java
@@ -1,13 +1,9 @@
 package com.jackwilsdon.PvPoints;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.bukkit.configuration.MemorySection;
 
 /*
